64a7f511e7fb06e2e0344f3633d8fda053fdf22e
JAEHO SONG <thruthesky@gmail.com>
2020-10-19 13:40:14 +0900

    adding user.token test

--- firebase/tests/helper.js
+++ firebase/tests/helper.js
@@ -1,10 +1,18 @@
 const firebase = require("@firebase/rules-unit-testing");
-const MY_PROJECT_ID = "social-management-system"; // 실제 Firebase Project ID.
+// 실제 Firebase Project ID.
+// Firebase Project ID.
+const MY_PROJECT_ID = "social-management-system";
+
+// Fake User UID - My UID.
 const myUid = "my_uid";
-const yourUid = "yourUid";
-const myAuth = { uid: myUid, email: "my@gmail.com" };
+// Fake User UiD
+const otherUid = "othe_uid";
+
+// Fake User Auth Data
+const myAuth = { uid: myUid, email: "my-email@gmail.com" };
 
 // Firestore intance 를 가져오는 함수
+// Get Firestore instance for CRUD work.
 function getFirestore(auth = null) {
   return firebase
     .initializeTestApp({ projectId: MY_PROJECT_ID, auth: auth })
@@ -12,18 +20,23 @@ function getFirestore(auth = null) {
 }
 
 // Firestore admin instance 를 가져오는 함수. 이 객체는 모든 권한을 다 가진다.
+// Get Firestore admin instance that has all the power(permission) on creat/update/delete of the entire Firestore.
+// Security rules does not apply with the admin instance.
 function getAdminFirestore() {
   return firebase.initializeAdminApp({ projectId: MY_PROJECT_ID }).firestore();
 }
 
+// Helper function to make the test code simple.
 module.exports.setup = async (auth, data) => {
-  await firebase.clearFirestoreData({ projectId: MY_PROJECT_ID });
+  // await firebase.clearFirestoreData({ projectId: MY_PROJECT_ID }); // Clear firestore on every setup.
   const db = getFirestore(auth); // Firestore instance 를 가져온다
   if (data) {
     // 데이터가 있으면,
+    // If there is data to set, set it with admin instance.
     const adminDb = getAdminFirestore(); // Admin DB 를 가져와서
     for (const key in data) {
       // key 마다, 데이터를 모두 기록한다.
+      // Save data for each path.
       const ref = adminDb.doc(key);
       await ref.set(data[key]);
     }
@@ -33,4 +46,4 @@ module.exports.setup = async (auth, data) => {
 
 module.exports.myAuth = myAuth;
 module.exports.myUid = myUid;
-module.exports.yourUid = yourUid;
+module.exports.otherUid = otherUid;
