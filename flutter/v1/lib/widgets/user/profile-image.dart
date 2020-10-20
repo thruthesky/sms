5e9392d374732954b0b6b019f568846532fe26e4
YinG17 <simbulanja.17@gmail.com>
2020-10-20 18:51:29 +0800

    refactor

--- flutter/v1/lib/widgets/user/profile-image.dart
+++ flutter/v1/lib/widgets/user/profile-image.dart
@@ -1,6 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:get/get.dart';
 import 'package:v1/controllers/user.controller.dart';
 import 'package:v1/widgets/spinner.dart';
 
