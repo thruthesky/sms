10b3136ac13f737c10be43998805a74eb056d470
YinG17 <simbulanja.17@gmail.com>
2020-10-22 17:47:06 +0800

    post like initial function

--- flutter/v1/lib/widgets/forum/post.dart
+++ flutter/v1/lib/widgets/forum/post.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:v1/controllers/user.controller.dart';
 import 'package:v1/services/models.dart';
 import 'package:v1/services/route-names.dart';
 import 'package:v1/services/service.dart';
@@ -17,8 +18,24 @@ class Post extends StatefulWidget {
 }
 
 class _PostState extends State<Post> {
-  final CollectionReference colPosts =
-      FirebaseFirestore.instance.collection('posts');
+  final UserController userController = Get.find();
+  final firestoreInstance = FirebaseFirestore.instance;
+
+  CollectionReference colPosts;
+  CollectionReference colLikes;
+
+  onVoteTap(String choice) async {
+    print('onVoteTap::choice => $choice');
+
+    /// TODO: VOTE SET/UPDATE
+  }
+
+  @override
+  void initState() {
+    colPosts = firestoreInstance.collection('posts');
+    colLikes = firestoreInstance.collection('likes');
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,16 +59,12 @@ class _PostState extends State<Post> {
             children: [
               IconButton(
                 icon: Icon(Icons.thumb_up),
-                onPressed: () {
-                  print('like');
-                },
+                onPressed: () => onVoteTap('like'),
               ),
               Text(widget.post.like.toString()),
               IconButton(
                 icon: Icon(Icons.thumb_down),
-                onPressed: () {
-                  print('dislike');
-                },
+                onPressed: () => onVoteTap('dislike'),
               ),
               Text(widget.post.dislike.toString()),
               if (Service.isMyPost(widget.post)) ...[
