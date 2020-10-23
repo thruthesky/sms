f88bdc164e8fdfeeaffe7c95fbf5211e141674c9
JaeHo Song <thruthesky@gmail.com>
2020-10-23 09:05:09 +0800

    update merging

--- flutter/v1/lib/screens/forum/forum.screen.dart
+++ flutter/v1/lib/screens/forum/forum.screen.dart
@@ -4,9 +4,11 @@ import 'package:after_layout/after_layout.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:v1/controllers/user.controller.dart';
 import 'package:v1/services/functions.dart';
 import 'package:v1/services/models.dart';
 import 'package:v1/services/route-names.dart';
+import 'package:v1/services/service.dart';
 import 'package:v1/services/spaces.dart';
 import 'package:v1/widgets/commons/spinner.dart';
 
@@ -230,7 +232,7 @@ class CommentEditForm extends StatefulWidget {
     Key key,
   }) : super(key: key);
 
-  final post;
+  final PostModel post;
 
   @override
   _CommentEditFormState createState() => _CommentEditFormState();
@@ -238,6 +240,7 @@ class CommentEditForm extends StatefulWidget {
 
 class _CommentEditFormState extends State<CommentEditForm> {
   final contentController = TextEditingController();
+  final user = Get.find<UserController>();
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -247,7 +250,23 @@ class _CommentEditFormState extends State<CommentEditForm> {
           decoration: InputDecoration(hintText: 'input comment'.tr),
         ),
         RaisedButton(
-          onPressed: () {},
+          onPressed: () async {
+            try {
+              // final postDoc = postDocument(widget.post.id);
+              final commentCol = commentCollection(widget.post.id);
+              print('ref.path: ' + commentCol.path.toString());
+              final data = {
+                'uid': user.uid,
+                'content': contentController.text,
+                'order': '00001.001.000.000.000.000.000.000.000.000.000.000',
+                'depth': 0,
+              };
+              print(data);
+              await commentCol.add(data);
+            } catch (e) {
+              Service.error(e);
+            }
+          },
           child: Text('submit'),
         )
       ],
