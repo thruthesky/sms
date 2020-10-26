659792eb9195d589c2d4af113f7c5ef8c79acf9b
JAEHO SONG <thruthesky@gmail.com>
2020-10-26 22:15:39 +0900

    update doc

--- firebase/tests/user.token.js
+++ firebase/tests/user.token.js
@@ -3,7 +3,6 @@ const { setup, myAuth, myUid, otherUid } = require("./helper");
 const tokenId = "token-1";
 
 describe("User Push Notification Token", () => {
-
   // users/{uid}/meta/tokens
 
   it("users/{uid}/meta/tokens Add token on other's account should failed", async () => {
@@ -12,7 +11,7 @@ describe("User Push Notification Token", () => {
       .collection("users")
       .doc(otherUid)
       .collection("meta")
-      .doc('tokens');
+      .doc("tokens");
     await firebase.assertFails(tokenDoc.set({ [tokenId]: true }));
   });
 
@@ -22,7 +21,7 @@ describe("User Push Notification Token", () => {
       .collection("users")
       .doc(myUid)
       .collection("meta")
-      .doc('tokens');
+      .doc("tokens");
     await firebase.assertSucceeds(tokenDoc.set({ [tokenId]: true }));
   });
   it("Read token should success", async () => {
@@ -34,36 +33,4 @@ describe("User Push Notification Token", () => {
       .doc("tokens");
     await firebase.assertSucceeds(tokenDoc.get());
   });
-
-
-  // /users/{uid}/tokens/{token}
-
-  // it("Add token on other's account should failed", async () => {
-  //   const db = await setup(myAuth);
-  //   const tokenDoc = db
-  //     .collection("users")
-  //     .doc(otherUid)
-  //     .collection("tokens")
-  //     .doc(tokenId);
-  //   await firebase.assertFails(tokenDoc.set({ token: tokenId }));
-  // });
-
-  // it("Add token should success", async () => {
-  //   const db = await setup(myAuth);
-  //   const tokenDoc = db
-  //     .collection("users")
-  //     .doc(myUid)
-  //     .collection("tokens")
-  //     .doc(tokenId);
-  //   await firebase.assertSucceeds(tokenDoc.set({ token: tokenId }));
-  // });
-  // it("Read token should success", async () => {
-  //   const db = await setup();
-  //   const tokenDoc = db
-  //     .collection("users")
-  //     .doc(otherUid)
-  //     .collection("tokens")
-  //     .doc("it-will-pass-since-token-is-redable");
-  //   await firebase.assertSucceeds(tokenDoc.get());
-  // });
 });
