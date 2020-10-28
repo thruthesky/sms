1bd82e1f7a4eb5962804ce7fbbce9d6aeeee271c
YinG17 <simbulanja.17@gmail.com>
2020-10-28 15:55:19 +0800

    update for using fireflutter

--- flutter/v1/lib/screens/forum/forum.screen.dart
+++ flutter/v1/lib/screens/forum/forum.screen.dart
@@ -50,10 +50,6 @@ class _ForumScreenState extends State<ForumScreen> {
       render: (x) => setState(() => null),
     );
 
-    // final args = routerArguments(context);
-    // category = args['category'];
-    // print('category ??: $category');
-
     /// Scroll event handler
     scrollController.addListener(() {
       // Check if the screen is scrolled to the bottom.
@@ -103,95 +99,6 @@ class _ForumScreenState extends State<ForumScreen> {
     forum.leave();
     super.dispose();
   }
-
-  // fetchPosts() {
-  //   if (inLoading || noMorePost) return;
-  //   setState(() => inLoading = true);
-  //   pageNo++;
-
-  //   Query postsQuery = colPosts.where('category', isEqualTo: category);
-  //   postsQuery = postsQuery.orderBy('createdAt', descending: true);
-  //   postsQuery = postsQuery.limit(10);
-
-  //   if (posts.isNotEmpty) {
-  //     postsQuery = postsQuery.startAfter([posts.last.createdAt]);
-  //   }
-
-  //   subscription = postsQuery.snapshots().listen((QuerySnapshot snapshot) {
-  //     // print('>> docChanges: ');
-
-  //     // TODO: do we really need to handle for snapshot.size == 0?
-  //     // if (snapshot.size == 0) { // return;
-
-  //     /// TODO: this produce `Unhandled Exception: Unimplemented handling of missing static target` exception seldomly.
-  //     snapshot.docChanges.forEach((DocumentChange documentChange) {
-  //       final data = documentChange.doc.data();
-  //       data['id'] = documentChange.doc.id;
-  //       final post = PostModel.fromDocument(data);
-
-  //       // print('Post:');
-  //       // print(post.toString());
-  //       // print('Document change type:');
-  //       // print(documentChange.type);
-
-  //       if (documentChange.type == DocumentChangeType.added) {
-  //         /// [createdAt] is null only on author's app since it is cached locally.
-  //         /// [createdAt] will not be null on other's app and will have the biggest value among other posts.
-  //         /// `modified` event will be fired right after with proper timestamp.
-  //         /// This will not be null on other's app and there will be no `modified` event on other's app.
-  //         ///
-  //         if (post.createdAt == null) {
-  //           posts.insert(0, post);
-  //         } else if (posts.isNotEmpty &&
-  //             post.createdAt.microsecondsSinceEpoch >
-  //                 posts[0].createdAt.microsecondsSinceEpoch) {
-  //           posts.insert(0, post);
-  //         } else {
-  //           posts.add(post);
-  //         }
-
-  //         /// Realtime update for the comments of the post
-  //         /// This will do only one time subscription since it is listening inside `added` event.
-  //         /// TODO Unsubscribe the comments of post.id or there will be double event firing when the user visit the forum and load the same comments of the post again.
-  //         commentsCollection(post.id)
-  //             .orderBy('order', descending: true)
-  //             .snapshots()
-  //             .listen((QuerySnapshot snapshot) {
-  //           snapshot.docChanges.forEach((DocumentChange commentsChange) {
-  //             // TODO: Do `CommentModel.fromDocument()`.
-  //             final commentData = commentsChange.doc.data();
-  //             final newComment = CommentModel.fromDocument(commentData);
-  //             if (commentsChange.type == DocumentChangeType.added) {
-  //               /// TODO For comments loading on post view, it does not need to loop.
-  //               /// TODO Only for newly created comment needs to have loop and find a position to insert.
-  //               int found = post.comments
-  //                   .indexWhere((c) => c.order.compareTo(newComment.order) < 0);
-  //               if (found == -1) {
-  //                 post.comments.add(newComment);
-  //               } else {
-  //                 post.comments.insert(found, newComment);
-  //               }
-  //             }
-  //             setState(() {});
-  //           });
-  //         });
-
-  //         inLoading =
-  //             false; // will be set to false many times. but does't matter.
-
-  //       } else if (documentChange.type == DocumentChangeType.modified) {
-  //         final int i = posts.indexWhere((p) => p.id == post.id);
-  //         if (i > 0) {
-  //           posts[i] = post;
-  //         }
-  //       } else if (documentChange.type == DocumentChangeType.removed) {
-  //         print('Removing post');
-  //         posts.removeWhere((p) => p.id == post.id);
-  //       }
-  //     });
-  //     setState(() {});
-  //   });
-  // }
 
   @override
   Widget build(BuildContext context) {
@@ -281,17 +188,16 @@ class _ForumScreenState extends State<ForumScreen> {
                   return Post(post: post);
                 },
               ),
-
-              if (!forum.inLoading)
+              if (forum.inLoading)
                 Padding(
                   padding: EdgeInsets.all(Space.md),
                   child: CommonSpinner(),
                 ),
-              // if (noMorePost)
-              //   Padding(
-              //     padding: EdgeInsets.all(Space.md),
-              //     child: Text('No more posts..'),
-              //   ),
+              if (forum.noMorePosts)
+                Padding(
+                  padding: EdgeInsets.all(Space.md),
+                  child: Text('No more posts..'),
+                ),
               // if (noPostsYet)
               //   Padding(
               //     padding: EdgeInsets.all(Space.md),
