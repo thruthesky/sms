f91f1c6b90e9c1bbb7183734a48a911482df2968
YinG17 <simbulanja.17@gmail.com>
2020-11-04 11:45:13 +0800

    app routing update

--- flutter/v1/lib/services/app-router.dart
+++ flutter/v1/lib/services/app-router.dart
@@ -13,17 +13,25 @@ class AppRouter {
   /// NOTE: `routing.removed` will always have a `String` value.
   ///   - empty string if the navigation event is not removing a route.
   static observer(Routing routing) {
-    /// remove route only when:
-    ///   1. the navigation event is moving to previous page.
-    ///   2. `Get.removeRoute` is called, thus, `routing.removed` will not be empty.
-    if (routing.isBack || routing.removed.isNotEmpty) {
+    print('Previous route : ${routing.previous}');
+    print('Current route : ${routing.current}');
+
+    /// if `routing.removed` is not empty, remove also from `navStack`.
+    if (routing.removed.isNotEmpty) {
+      print('Route is removed!! ${routing.removed}');
+      navStack.remove(routing.removed);
+      Get.routing.args = null;
+      return;
+    }
+
+    /// if navigation event is going to previous page, we only remove previous route from `navStack`.
+    if (routing.isBack) {
       /// get the previous route name and remove it from stack.
       final routeName = getRouteName(routing, previous: true);
+      print('Previous route is removed!! $routeName');
       navStack.remove(routeName);
+      Get.routing.args = null;
     } else {
-      /// ignore if `routing.removed` is not empty, since it is handled above.
-      if (routing.removed.isNotEmpty) return;
-
       final routeName = getRouteName(routing);
       final Route navRoute = navStack[routeName];
 
@@ -38,7 +46,10 @@ class AppRouter {
 
       /// add route to `navStack`
       navStack[routeName] = routing.route;
+      print('Route is added!! $routeName');
     }
+
+    print('Nav Stack : $navStack');
   }
 
   /// if `previous` is set to true, it will use the `previous` route name instead of `current`.
@@ -49,7 +60,7 @@ class AppRouter {
     String routeName = previous ? routing.previous : routing.current;
 
     /// if the route name contains the forum route name, it adds the category to `routeName`.
-    if (routeName.contains(RouteNames.forum)) {
+    if (routeName == RouteNames.forum) {
       dynamic args = routing.args;
       routeName += args['category'];
     }
